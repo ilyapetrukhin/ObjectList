78af113a340ce7ab0f1f6410e443f481490e595f
Ilya Petrukhin <petrukhinilya61@list.ru>
2022-08-07 00:05:21 +0300

    Create base connections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-
-
+import { useActions } from './hooks/useActions';
+import { useTypedSelector } from './hooks/useTypedSelector';
 
 interface Vehicles {
   id: number,
@@ -22,7 +22,13 @@ interface Truck {
 }
 
 
-function App() {
+const App:  React.FC = () =>  {
+  const {fetchUsers} = useActions()
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
   const vehicles: Vehicles[] = [
     {
     "id": 1,
@@ -61,21 +67,41 @@ function App() {
         },
         },
     ];
+
+    const {users, error, loading} = useTypedSelector(state => state.user)
+
+    console.log(users)
+
+    if (loading) {
+      return <h1>Идет загрузка...</h1>
+  }
+  if (error) {
+      return <h1>{error}</h1>
+  }
+    
   return (
-    <div className="App">
-      {vehicles.map((el: Vehicles)=> {
-        return <div>
+    <>
+      {vehicles.map((el)=> {
+        return <div key={el.id}>
           <p>
           {el.type}
           </p>
-          <p>
+          {/* <p>
           {el.model.seats || el.model.wheelDrive} --
           {el.model.doors || el.model.ecoClass}
-          </p>
+          </p> */}
         </div>
       })
     }
-    </div>
+    {
+      users && users.map((user) => {
+        return (
+          <div key={user.id}>{user.name}</div>
+
+        )
+      })
+    }
+    </>
   );
 }
 
